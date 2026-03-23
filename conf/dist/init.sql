417d299d7fca36cb152dd08e56f2117bd284ae39
Yehonal <yehonal.azeroth@gmail.com>
2026-03-23 09:27:07 +0000

    feat: add MySQL initialization script and update docker-compose for acore user

---
+++ conf/dist/init.sql
@@ -0,0 +1,7 @@
+DROP USER IF EXISTS 'acore'@'localhost';
+DROP USER IF EXISTS 'acore'@'127.0.0.1';
+CREATE USER 'acore'@'localhost' IDENTIFIED BY 'acore';
+CREATE USER 'acore'@'127.0.0.1' IDENTIFIED BY 'acore';
+GRANT ALL PRIVILEGES ON * . * TO 'acore'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON * . * TO 'acore'@'127.0.0.1' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
